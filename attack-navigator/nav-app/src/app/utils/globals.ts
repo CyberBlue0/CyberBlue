9946d2f5c5d15a7dad7ec142f1a957e2252b8fa8
CyberBlue0 <basem.saleh012@gmail.com>
2025-09-07 20:12:31 +0300

    Delete attack-navigator directory

--- attack-navigator/nav-app/src/app/utils/globals.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-import appPackageInfo from '../../../package.json';
-
-export const navVersion: string = appPackageInfo.version;
-export const layerVersion: string = '4.5';
-export const minimumSupportedVersion: string = '4.0';
